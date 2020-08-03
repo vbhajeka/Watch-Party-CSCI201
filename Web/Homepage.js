2d5fa163dc7403a41f8049099dfe0c272388d242
Henry He <hejiayan@usc.edu>
2020-08-02 19:38:16 -0700

    first commit

---
+++ Web/Homepage.js
@@ -0,0 +1,138 @@
+/* popupcreate*/
+function showPopupCreate(){
+	var popc=document.getElementById("pop-up-box-container-create");
+	var pop=document.getElementById("pop-up-box-create");
+	popc.style.display="block";
+	pop.style.display="block";
+
+	var winWidth=window.innerWidth;
+			
+	pop.style.left=(winWidth/2)-150+"px";
+	pop.style.top="300px";
+}
+
+function popupOkCreate(){
+	popupHideCreate();
+}
+
+function popupCancelCreate(){
+	popupHideCreate();
+}
+
+function popupHideCreate(){
+	var popc=document.getElementById("pop-up-box-container-create");
+	var pop=document.getElementById("pop-up-box-create");
+	popc.style.display="none";
+	pop.style.display="none";
+}
+
+/* popupjoin*/
+function showPopupJoin(){
+	var popc=document.getElementById("pop-up-box-container-join");
+	var pop=document.getElementById("pop-up-box-join");
+	popc.style.display="block";
+	pop.style.display="block";
+
+	var winWidth=window.innerWidth;
+			
+	pop.style.left=(winWidth/2)-150+"px";
+	pop.style.top="300px";
+}
+
+function popupOkJoin(){
+	popupHideJoin();
+}
+
+function popupCancelJoin(){
+	popupHideJoin();
+}
+
+function popupHideJoin(){
+	var popc=document.getElementById("pop-up-box-container-join");
+	var pop=document.getElementById("pop-up-box-join");
+	popc.style.display="none";
+	pop.style.display="none";
+}
+
+/*popupleave*/
+function showPopupLeave(){
+	var popc=document.getElementById("pop-up-box-container-leave");
+	var pop=document.getElementById("pop-up-box-leave");
+	popc.style.display="block";
+	pop.style.display="block";
+
+	var winWidth=window.innerWidth;
+			
+	pop.style.left=(winWidth/2)-150+"px";
+	pop.style.top="300px";
+}
+
+function popupOkLeave(){
+	popupHideLeave();
+}
+
+function popupCancelLeave(){
+	popupHideLeave();
+}
+
+function popupHideLeave(){
+	var popc=document.getElementById("pop-up-box-container-leave");
+	var pop=document.getElementById("pop-up-box-leave");
+	popc.style.display="none";
+	pop.style.display="none";
+}
+
+/*popuplogout*/
+function showPopupLogout(){
+	var popc=document.getElementById("pop-up-box-container-logout");
+	var pop=document.getElementById("pop-up-box-logout");
+	popc.style.display="block";
+	pop.style.display="block";
+
+	var winWidth=window.innerWidth;
+			
+	pop.style.left=(winWidth/2)-150+"px";
+	pop.style.top="300px";
+}
+
+function popupOkLogout(){
+	popupHideLogout();
+}
+
+function popupCancelLogout(){
+	popupHideLogout();
+}
+
+function popupHideLogout(){
+	var popc=document.getElementById("pop-up-box-container-logout");
+	var pop=document.getElementById("pop-up-box-logout");
+	popc.style.display="none";
+	pop.style.display="none";
+}
+
+
+
+/*popupsearch*/
+function showPopupSearch(){
+	var popc=document.getElementById("pop-up-box-container-search");
+	var pop=document.getElementById("pop-up-box-search");
+	popc.style.display="block";
+	pop.style.display="block";
+
+	var winWidth=window.innerWidth;
+			
+	pop.style.left=(winWidth/2)-250+"px";
+	pop.style.top="150px";
+}
+
+function popupCancelSearch(){
+	popupHideSearch();
+}
+
+function popupHideSearch(){
+	var popc=document.getElementById("pop-up-box-container-search");
+	var pop=document.getElementById("pop-up-box-search");
+	popc.style.display="none";
+	pop.style.display="none";
+}
+
