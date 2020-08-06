87bc4d0d726695cc2b81b5fc82a8d7acaf5a49c2
Alex Balasky <abalasky@gmail.com>
2020-08-05 18:12:58 -0700

    Popup work

--- watchparty/src/main/resources/static/js/watchroom/voting.js
+++ watchparty/src/main/resources/static/js/watchroom/voting.js
@@ -14,16 +14,27 @@ function main() {
 
             let response = await fetch(`http://localhost:8081/api/youtubeSearch?query=${userSearch}`);
 
-            if(response.ok) {
-                let json = await response.json();
-            }
-            else {
-                console.log("Error");
+            let json = await response.json();
+
+            //Add videos to the DOM
+
+            //Loop thru each video in the response
+            for(let video of json) {
+                let videoTitle = video.videoTitle;
+                let thumbnail = video.thumbnail;
+
+                //Append to popup
+                let html = `<div class="youtube-result-container">
+                    <img
+                        class ="yt-thumbnail"
+                        src="https://static.politico.com/dims4/default/e47f863/2147483647/resize/1160x%3E/quality/90/?url=https%3A%2F%2Fstatic.politico.com%2Fbe%2Ffd%2Ffa12678b4813a0ba11122c146100%2Fgettyimages-1222600015.jpg" alt="">
+                        <p class="yt-title">${videoTitle}</p>
+                        <button type="button" class="btn-lg btn-success" id="voteButton">Vote
+                        </button>
+                </div>`
             }
 
-            console.log(json);
 
-            //Apppend videos to DOM
 
             showPopupSearch();
         });
